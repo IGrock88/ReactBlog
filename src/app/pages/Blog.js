ab3307caae4018883f4a70cce3397774eb5498a9
IGrock88 <igrock88@gmail.com>
2018-05-11 21:36:56 +0800

    refactoring

---
+++ src/app/pages/Blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Blog extends React.Component {
+
+    render() {
+
+        return (
+            <main className="col-lg-8 left-col">
+                <div className="panel panel-danger">Hello, тут будут блоги</div>
+            </main>
+        );
+    }
+
+}
